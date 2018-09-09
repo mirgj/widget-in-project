69c2252319ced2c8fe44a1a64664096b84245f1e
mirgj <mirash.gjolaj@gmail.com>
2018-09-09 23:47:36 +0800

    new conf

--- nextjs-starter/next.config.js
+++ nextjs-starter/next.config.js
@@ -1,3 +1,5 @@
+const TransferWebpackPlugin = require('transfer-webpack-plugin');
+
 module.exports = {
   webpack: (config, { dev }) => {
     config.module.rules.push(
@@ -16,7 +18,28 @@ module.exports = {
         test: /\.scss$/,
         loader: 'babel-loader!raw-loader!sass-loader'
       }
-    )
+    );
+    config.module.rules.push({
+      test: /\.(jpe?g|png|svg|gif|ico)$/,
+      use: [
+        {
+          loader: "url-loader",
+          options: {
+            limit: 1,
+            fallback: "file-loader",
+            publicPath: `/_next/static/media/`,
+            outputPath: `static/media/`,
+            name: "[name].[hash].[ext]"
+          }
+        }
+      ]
+    });
+
+    config.plugins.push(
+      new TransferWebpackPlugin([
+        { from: 'node_modules/fakeloader/build/static', to: 'static/' }
+      ])
+    );
     return config
   }
 }
